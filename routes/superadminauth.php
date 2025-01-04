702b5c998e4a39234f942b6c91e293f2fed9a480
Prath@7057 <iamprathpatil@gmail.com>
2025-01-04 22:43:10 +0530

    added Super admin route, views and controllers

---
+++ routes/superadminauth.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\SuperAdmin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\SuperAdmin\ProfileController;
+use App\Http\Controllers\Auth\PasswordController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('SuperAdmin')->name('SuperAdmin')->group(function () {
+
+    Route::middleware('guest')->group(function () {
+
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ->name('login');
+
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    });
+
+    Route::middleware('auth')->group(function () {
+
+        Route::get('/dashboard', function () {
+            return view('SuperAdmin.dashboard');
+        })->name('dashboard');
+
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+        Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
+        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->name('logout');
+    });
+});
