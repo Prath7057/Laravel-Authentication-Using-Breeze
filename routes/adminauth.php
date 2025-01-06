6bfb1db14eaa5fe28b172dca93780f3470f14151
Prath@7057 <iamprathpatil@gmail.com>
2025-01-06 22:28:53 +0530

    added admin authentication at 05 Jan -Prathamesh

---
+++ routes/adminauth.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Auth\PasswordController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('Admin')->name('Admin.')->group(function () {
+
+    Route::middleware('guest')->group(function () {
+
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ->name('login');
+
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    });
+
+    Route::middleware('auth:super_admin')->group(function () {
+
+        Route::get('/dashboard', function () {
+            return view('Admin.dashboard');
+        })->name('dashboard');
+
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+        Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
+        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->name('logout');
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 require __DIR__.'/superadminauth.php';
+require __DIR__.'/adminauth.php';
